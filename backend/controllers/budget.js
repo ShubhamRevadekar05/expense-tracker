f987afe5dd0592578358d34962cde4167943be4a
unknown <shubham0revadekar@gmail.com>
2023-10-26 13:47:56 +0530

    changed budget api for updating budgets

--- backend/controllers/budget.js
+++ backend/controllers/budget.js
@@ -10,22 +10,34 @@ exports.addBudget = async (req, res) => {
         let userDetails = jwt.verify(req.headers["authorization"].split("Bearer ")[1], SECRET_KEY);
         let user = await UserSchema.findOne({username: userDetails.username, email: userDetails.email}).exec();
         if(user) {
-            const budget = BudgetSchema({
-                amount,
-                category,
-                userId: user.id
-            })
-        
+            
             try {
                 //validations
-                if(!category){
-                    return res.status(400).json({message: 'All fields are required!'})
+                let budget = await BudgetSchema.findOne({category: category}).exec();
+                if(budget) {
+                    budget = BudgetSchema({
+                        amount,
+                        category,
+                        userId: user.id
+                    })
+                
+                    if(!category){
+                        return res.status(400).json({message: 'All fields are required!'})
+                    }
+                    if(amount <= 0 || !amount === 'number'){
+                        return res.status(400).json({message: 'Amount must be a positive number!'})
+                    }
+                    await budget.save()
+                    res.status(200).json({message: 'Budget Added'})
+                
                 }
-                if(amount <= 0 || !amount === 'number'){
-                    return res.status(400).json({message: 'Amount must be a positive number!'})
+                else {
+                    if(amount <= 0 || !amount === 'number'){
+                        return res.status(400).json({message: 'Amount must be a positive number!'})
+                    }
+                    await BudgetSchema.findByIdAndUpdate(budget.id, {amount: amount}).exec()
+                    res.status(200).json({message: 'Budget Updated'})
                 }
-                await budget.save()
-                res.status(200).json({message: 'Budget Added'})
             } catch (error) {
                 res.status(500).json({message: 'Server Error'})
             }
